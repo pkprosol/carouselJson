99bfc9f64e446280bb00ff2be7a9ea63b1d37daa
yuralala8 <yc1435@nyu.edu>
2018-03-26 16:50:42 -0400

    carousel fetching data from the speedbump api

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Carousel } from 'react-bootstrap';
 import { Button } from 'react-bootstrap';
 import data from './data.json';
+import SpeedbumpData from './SpeedbumpData.js'
 import './App.css';
 
 class App extends Component {
@@ -9,24 +10,34 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-
+        realdata: []
     }
   }
 
-  render() {
+  myCallback = (dataFromChild) => {
+    if (this.state.realdata !== dataFromChild) {
+      this.setState({
+      realdata: dataFromChild
+      })
+    }
 
-    const speedBumpList = data.map((list,i) => {
+  }
+
+  render() {
+    console.log("realdata", this.state.realdata)
+
+    const speedBumpList = this.state.realdata.map((list,i) => {
       return (
         <Carousel.Item key={i}>
-          <img width={'100%'} height={'100%'} alt="900x500" src={list.img} />
+          <img width={'100%'} height={'100%'} alt="900x500" src="https://images.pexels.com/photos/775416/pexels-photo-775416.jpeg" />
           <div className="date">
-            <h4>{list.date}</h4>
+            <h4>{list.title}</h4>
           </div>
           <Carousel.Caption>
             <h3>{list.title}</h3>
             <p>{list.description}</p>
-            <Button bsStyle="primary" href={list.link} target="_parent" style={{ marginBottom: 30 }}>
-              Try It Now
+            <Button bsStyle="primary" href={"https://app.quizzify.com/speedbumps/" + list.slug} target="_parent" style={{ marginBottom: 30 }}>
+              Read More
 				  </Button>
           </Carousel.Caption>
         </Carousel.Item>
@@ -34,9 +45,12 @@ class App extends Component {
     })
 
     return (
+      <div>
       <Carousel>
        {speedBumpList}
       </Carousel>
+      <SpeedbumpData callbackFromParent={this.myCallback} />
+      </div>
     );
   }
 }
